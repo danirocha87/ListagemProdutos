4bfbb93f178ce01ffa1a161702310df7394bc7e3
daniela oliveira <danioliver_17@hotmail.com>
2023-11-01 19:02:54 -0300

    instalando o Swlltalert2

--- src/app/pages/cadastrar-produto/cadastrar-produto.component.ts
+++ src/app/pages/cadastrar-produto/cadastrar-produto.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { ProdutoService } from './../../services/produto.service';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { IProduto } from 'src/app/interfaces/produto';
 
 @Component({
   selector: 'app-cadastrar-produto',
@@ -16,7 +17,17 @@ export class CadastrarProdutoComponent {
       CodigoDeBarras: new FormControl(''),
       preco: new FormControl(''),
     });
-    enviar() {
-      console.log(this.produtoForm.value)
-  }
+    Enviar() {
+      const produto: Partial<IProduto> 
+      = this.produtoForm.value as IProduto;
+       produto.ativo= true; 
+  
+       console.log(produto)
+       
+       this.ProdutoService.cadastrarProduto(Produto)
+       .subscribe(result =>{
+        console.log(result)
+       }
+        )
+    } 
 }
